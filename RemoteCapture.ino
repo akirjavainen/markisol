06779fee5bfaa8f4a9dc122abe6220ee1edd3655
akirjavainen <antti.kirjavainen@justinwood.fi>
2018-10-30 23:16:01 +0200

    Update RemoteCapture.ino

--- RemoteCapture.ino
+++ RemoteCapture.ino
@@ -2,17 +2,22 @@
 // Plug your 433.92MHz receiver to digital pin 2:
 #define RECEIVE_PIN   2
 
-// Enable if there's no serial output or if you're modifying this code for another protocol/device:
-#define DEBUG         false   
+// Enable debug mode if there's no serial output or if you're modifying this code for
+// another protocol/device. However, note that serial output delays receiving, causing
+// capture to fail. So keep debug disabled unless absolutely required:
+#define DEBUG         false
+#define ADDITIONAL    false    // Display some additional information after capture
 
 
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 void setup()
 {
   pinMode(RECEIVE_PIN, INPUT);
   Serial.begin(9600);
 }
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 void loop()
 {
   int i = 0;
@@ -71,11 +76,11 @@ void loop()
       Serial.print(": ");
     }
 
-    if (t > 728 && t < 760) { // Long spikes signal 1
+    if (t > 560 && t < 760) { // Long spikes signal 1
       command += "1";
       if (DEBUG) Serial.println("1");
       
-    } else if (t > 330 && t < 360) { // Short spikes signal 0
+    } else if (t > 290 && t < 390) { // Short spikes signal 0
       command += "0";
       if (DEBUG) Serial.println("0");
       
@@ -99,9 +104,85 @@ void loop()
 
   // Correct command length is 41 bits, dismiss bad captures:
   if (command.length() < 41 || command.length() > 41) {
-    Serial.println("Bad capture, invalid command length.");
+    Serial.print("Bad capture, invalid command length ");
+    Serial.println(command.length());
+    if (ADDITIONAL) Serial.println("Invalid command: " + command);
+    Serial.println();
+    
   } else {
-    Serial.print("Successful capture, command is: ");
-    Serial.println(command);
+    Serial.println("Successful capture, command is: " + command);
+    Serial.println("Remote control (unique) ID: " + command.substring(0, 17));
+    Serial.println("Channel: " + printChannel(command.substring(17, 21)));
+    Serial.println("Command: " + printCommand(command.substring(21, 25)));
+    Serial.println("Remote control model: " + printRemoteModel(command.substring(25, 33)));
+    if (ADDITIONAL) Serial.println("Checksum (CRC): " + command.substring(33, 41));
+    Serial.println();
   }
 }
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+String printChannel(String channel) {
+
+  if (channel == "0111") {
+      return "1"; // ...or remote model BF-301
+
+  } else if (channel == "1011") {
+      return "2"; // ...or remote model BF-101
+
+  } else if (channel == "0011") {
+      return "3";
+
+  } else if (channel == "1101") {
+      return "4";
+
+  } else if (channel == "0101") {
+      return "5";
+  }
+
+  return "UNKNOWN";
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+String printCommand(String command) {
+
+  if (command == "0111") {
+      return "DOWN";
+
+  } else if (command == "1100") {
+      return "UP";
+
+  } else if (command == "0101") {
+      return "STOP";
+
+  } else if (command == "1101") {
+      return "PAIR/CONFIRM (C)";
+
+  } else if (command == "1011") {
+      return "PROGRAM LIMITS (L)";
+
+  } else if (command == "1110") {
+      return "CHANGE DIRECTION OF ROTATION (STOP + L)";
+  }
+
+  return "UNKNOWN";
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+String printRemoteModel(String model) {
+
+  if (model == "01111001") {
+      return "BF-305 (5 channels)";
+  
+  } else if (model == "11111100") {
+      return "BF-101 (single channel)";
+      
+  } else if (model == "01111100") {
+      return "BF-301 (single channel)";
+  }
+
+  return "UNKNOWN/NEW";
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
