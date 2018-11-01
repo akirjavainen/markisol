23a00e54773e1885e11c4bb66347cfe8ec461454
akirjavainen <antti.kirjavainen@justinwood.fi>
2018-11-01 11:50:35 +0200

    Create SerialRead.ino

---
+++ SerialRead.ino
@@ -0,0 +1,126 @@
+/*
+******************************************************************************************************************************************************************
+*
+* Arduino serial read example
+* Receiving commands from serial, eg. connecting Raspberry Pi or
+* other systems with Arduino
+*
+* HOW TO USE
+* Create a bash script called "433trigger.sh" to simplify sending commands:
+
+
+#!/bin/bash
+export DEVICE=/dev/ttyACM0 # Could also be /dev/ttyUSB0 on some systems
+
+if [ ! -n "$1" ]; then echo "Usage: $0 [command]"; exit; fi
+
+echo "<PREPARE>" >$DEVICE # Send some extra data to make sure serial reacts
+echo "<$1>" >$DEVICE
+
+
+* Then trigger with "433trigger.sh MY_OWN_COMMAND".
+* Add your commands and actions to show_new_data_received().
+*
+* Note that if your Arduino shows up as ttyUSB* (eg. ttyUSB0), you may need to
+* "cat /dev/ttyUSB0" on another terminal for the serial interface to react.
+* This is related to the serial protocol itself.
+*
+******************************************************************************************************************************************************************
+*/
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+const byte char_count = 32;
+
+char received_data[char_count];
+boolean new_data_received = false;
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+void setup()
+{
+  Serial.begin(9600);
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+void loop()
+{
+  receive_new_data();
+  show_new_data_received(); // Required in 2 places for serial read to work
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+void show_new_data_received() {
+  
+    if (new_data_received) {
+      new_data_received = false;
+
+      Serial.println(received_data);
+
+      // Add your own commands here and what to do with them (without the headers <>):
+      if (char_array_contains(received_data, "MY_OWN_COMMAND") > 0) {
+        Serial.println("My own command received.");
+      }
+    } 
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+void receive_new_data() {
+    char marker_header = '<';
+    char marker_finish = '>';
+    char current_char;
+    static byte b = 0;
+    static boolean receive_in_progress = false;
+ 
+     while (Serial.available() > 0 && new_data_received == false) {
+        current_char = Serial.read();
+
+        if (receive_in_progress == true) {
+            if (current_char != marker_finish) {
+                received_data[b] = current_char;
+                b++;
+
+                if (b >= char_count) b = char_count - 1;
+
+            } else {
+                received_data[b] = '\0'; // String termination
+                receive_in_progress = false;
+                b = 0;
+                new_data_received = true;
+                show_new_data_received(); // Required here and in loop()
+            }
+
+        } else if (current_char == marker_header) {
+            receive_in_progress = true;
+        }
+    }
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+char char_array_contains(char *str, char *str_find) {
+    char i = 0;
+    char found = 0;
+    char length = strlen(str);
+    
+    if (strlen(str_find) > length) return 0;
+
+    while (i < length) {
+        if (str[i] == str_find[found]) {
+            found++;       
+      if (strlen(str_find) == found) return 1;
+
+        } else {
+            found = 0;
+        }
+
+        i++;
+    }
+    return 0;
+}
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
